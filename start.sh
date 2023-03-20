9bb70a41fa6afc726e9480ec77ee19ced169a52f
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-03-20 22:11:20 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ git clone https://github.com/konichiwa55115/telminal /LazyDeveloper
 cd /LazyDeveloper
 pip3 install -r requirements.txt
 echo "Starting Bot...."
-python3 -m telminal
+gunicorn app:app && python3 -m telminal
