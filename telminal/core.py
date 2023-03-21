992b90220f838a8fc6f5b9a079e9da591bd8c012
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-03-21 09:56:34 +0200

    Update core.py

--- telminal/core.py
+++ telminal/core.py
@@ -61,7 +61,7 @@ class TProcess:
         while True:
             try:
                 line = self._process.read_nonblocking(size=1000, timeout=0)
-                self._buffer.write(line.decode("utf-8"))
+                self._buffer.write(line.decode('utf-8','ignore'))
                 self._new_data = True
             except EOF:
                 self.done()
